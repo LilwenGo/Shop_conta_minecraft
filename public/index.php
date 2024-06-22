f25f88b5c7dd3484cad8acbb17a2dc46266f26e9
BluffDragon8211 <lortega@edenschool.fr>
2024-06-22 14:50:11 +0200

    Import MVC Template

---
+++ public/index.php
@@ -0,0 +1,12 @@
+<?php
+
+session_start();
+
+require '../src/config/config.php';
+require '../vendor/autoload.php';
+require SRC . 'helper.php';
+
+$router = new Project\Router($_SERVER["REQUEST_URI"]);
+$router->get('/', "TodoController@index");
+
+$router->run();
