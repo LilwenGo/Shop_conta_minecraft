2f2cf39cb36ecb372740e1998238f934f62b2f19
BluffDragon8211 <lortega@edenschool.fr>
2024-06-28 11:53:28 +0200

    Documenting controllers

--- src/Controllers/Controller.php
+++ src/Controllers/Controller.php
@@ -2,15 +2,29 @@
 namespace Project\Controllers;
 use Project\Validator;
 
+/**Class Controller */
 abstract class Controller {
+    /**
+     * The controller's manager
+     */
     protected Object $manager;
+    /**
+     * Validator
+     */
     protected Validator $validator;
 
+    /**
+     * Instantiate the validator
+     */
     public function __construct() {
         $this->validator = new Validator();
     }
 
-    static public function render(string $view, array $data = []) {
+    /**
+     * Render the passed view
+     * $data is optional, if you want to send a variable to the view you have to send it in the data: ['variable' => value] 
+     */
+    static public function render(string $view, array $data = []): void {
         extract($data);
 
         ob_start();
--- src/Controllers/TeamController.php
+++ src/Controllers/TeamController.php
@@ -2,12 +2,19 @@
 namespace Project\Controllers;
 use Project\Models\TeamManager;
 
+/**Class TeamController */
 class TeamController extends Controller {
+    /**
+     * Init the manager and call the parent's constructor
+     */
     public function __construct() {
         $this->manager = new TeamManager();
         parent::__construct();
     }
 
+    /**
+     * Render the homepage
+     */
     public function index(): void {
         Controller::render('index');
     }
