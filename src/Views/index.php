22148276dd89b4d9a3bf3e2481c49c488ec2585c
BluffDragon8211 <lortega@edenschool.fr>
2024-07-02 18:29:49 +0200

    Adding buttons style

---
+++ src/Views/index.php
@@ -0,0 +1,6 @@
+<section class="section-yellow section-flex-y">
+    <h1>Shop conta for minecraft</h1>
+    <p>Bienvenue sur ce site de conta pour les shops de minecraft !</p>
+    <a href="/team/register" class="btn-purple">Créer une équipe</a>
+    <a href="/team/login" class="btn-blue">Se connecter</a>
+</section>
