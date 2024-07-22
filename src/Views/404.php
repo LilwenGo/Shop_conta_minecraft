dfc56790013eb1a208a80536b21851a71d3bfbc9
BluffDragon8211 <lortega@edenschool.fr>
2024-07-22 13:49:17 +0200

    Changing section color

--- src/Views/404.php
+++ src/Views/404.php
@@ -1,4 +1,4 @@
-<section class="error section-yellow">
-    <h1>Erreur 404</h1>
-    <p>La page recherchée n'existe pas ! <a href="/">Quitter cette page !</a></p>
+<section class="section-purple">
+    <h1 class="error">Erreur 404</h1>
+    <p>La page recherchée n'existe pas ! <a href="/" class="text-xs">Quitter cette page !</a></p>
 </section>
--- src/Views/index.php
+++ src/Views/index.php
@@ -1,6 +1,6 @@
-<section class="section-yellow section-flex-y">
-    <h1>Shop conta for minecraft</h1>
-    <p>Bienvenue sur ce site de conta pour les shops de minecraft !</p>
-    <a href="/team/register" class="btn-purple">Créer une équipe</a>
+<section class="section-purple section-flex-y">
+    <h1>Shop compta for minecraft</h1>
+    <p>Bienvenue sur ce site de compta pour les shops de minecraft !</p>
+    <a href="/team/register" class="btn-yellow">Créer une équipe</a>
     <a href="/team/login" class="btn-blue">Se connecter</a>
 </section>
