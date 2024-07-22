c0503e82a69c51e4f19e6e3026009e1403672068
BluffDragon8211 <lortega@edenschool.fr>
2024-07-22 13:47:07 +0200

    Adding routes

--- public/index.php
+++ public/index.php
@@ -7,6 +7,31 @@ require '../vendor/autoload.php';
 require SRC . 'helper.php';
 
 $router = new Project\Router($_SERVER["REQUEST_URI"]);
+//Home
 $router->get('/', "TeamController@index");
+
+//Team
+$router->get('/team/', "TeamController@show");
+$router->get('/team/attempt/', "TeamController@update");
+$router->get('/team/logout/', "TeamController@logout");
+$router->get('/team/login', "TeamController@showLogin");
+$router->post('/team/login/attempt/', "TeamController@login");
+$router->get('/team/register', "TeamController@showRegister");
+$router->post('/team/register/attempt/', "TeamController@register");
+
+//Admins
+$router->get('/admins/', "AdminController@index");
+$router->post('/admins/create/', "AdminController@store");
+$router->post('/admins/:id/update/', "AdminController@update");
+$router->get('/admins/:id/delete/', "AdminController@delete");
+$router->get('/admins/login', "AdminController@showLogin");
+$router->post('/admins/login/attempt/', "AdminController@login");
+$router->get('/admins/logout', "AdminController@logout");
+
+//Membres
+$router->get('/membres/', "MembreController@index");
+$router->post('/membres/create/', "MembreController@store");
+$router->post('/membres/:id/update/', "MembreController@update");
+$router->post('/membres/:id/delete/', "MembreController@delete");
 
 $router->run();
