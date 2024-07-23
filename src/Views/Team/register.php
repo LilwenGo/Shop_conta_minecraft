727141d326d8474991a78e4255211a1d28685f62
BluffDragon8211 <lortega@edenschool.fr>
2024-07-23 14:06:45 +0200

    Minor fixes

--- src/Views/Team/register.php
+++ src/Views/Team/register.php
@@ -1,11 +1,12 @@
 <section class="section-purple section-flex-y">
     <h1>Créer une équipe</h1>
+    <p class="text-xs important">Attention: le nom d'équipe ne pourra pas être modifié !</p>
     <form class="form" action="/team/register/attempt" method="post" enctype="multipart/form-data">
         <label for="name" class="text-s">Nom d'équipe:</label>
         <input type="text" name="name" id="name" value="<?= old('name');?>" class="input" autocomplete="off">
         <span class="error text-xs"><?= error('name');?></span>
         <label for="password" class="text-s">Mot de passe:</label>
-        <input type="password" name="password" id="password" value="<?= old('password');?>" class="input">
+        <input type="password" name="password" id="password" value="<?= old('password');?>" class="input" autocomplete="current-password">
         <span class="error text-xs"><?= error('password');?></span>
         <input type="submit" value="Valider" name="ok" class="btn-blue">
     </form>
