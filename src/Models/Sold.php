920ed757898e15835dbc2994333810e62a6beabc
BluffDragon8211 <lortega@edenschool.fr>
2024-08-07 17:55:31 +0200

    Adding solds

---
+++ src/Models/Sold.php
@@ -0,0 +1,59 @@
+<?php
+namespace Project\Models;
+
+class Sold {
+    private int $id_item;
+    private string $item;
+    private int $id_membre;
+    private string $membre;
+    private int $quantity;
+    private int $refunded;
+
+    public function getId_item(): int {
+        return $this->id_item;
+    }
+    
+    public function setId_item(int $id_item): void {
+        $this->id_item = $id_item;
+    }
+    
+    public function getItem(): string {
+        return $this->item;
+    }
+    
+    public function setItem(string $item): void {
+        $this->item = $item;
+    }
+
+    public function getId_membre(): int {
+        return $this->id_item;
+    }
+    
+    public function setId_membre(int $id_membre): void {
+        $this->id_membre = $id_membre;
+    }
+    
+    public function getMembre(): string {
+        return $this->membre;
+    }
+    
+    public function setMembre(string $membre): void {
+        $this->membre = $membre;
+    }
+
+    public function getQuantity(): int {
+        return $this->quantity;
+    }
+    
+    public function setQuantity(int $quantity): void {
+        $this->quantity = $quantity;
+    }
+
+    public function getRefunded(): int {
+        return $this->refunded;
+    }
+    
+    public function setRefunded(int $refunded): void {
+        $this->refunded = $refunded;
+    }
+}
---
+++ src/Models/SoldManager.php
@@ -0,0 +1,88 @@
+<?php
+namespace Project\Models;
+
+/** Class SoldManager */
+class SoldManager extends Manager {
+    /**
+     * Return all the items
+     */
+    public function getAll(): array {
+        $stmt = $this->db->query('SELECT id_item, item.libelle AS item, id_membre, membre.name AS membre, quantity, refunded FROM item_membre JOIN item ON item.id = item_membre.id_item JOIN membre ON membre.id = item_membre.id_membre');
+        return $stmt->fetchAll(\PDO::FETCH_CLASS, Sold::class);
+    }
+
+    /**
+     * Return the sold with matching id_item
+     */
+    public function find(int $id_item, int $id_membre): array {
+        $stmt = $this->db->prepare('SELECT id_item, item.libelle AS item, id_membre, membre.name AS membre, quantity, refunded FROM item_membre JOIN item ON item.id = item_membre.id_item JOIN membre ON membre.id = item_membre.id_membre WHERE id_item = ? AND id_membre = ?');
+        $stmt->execute([
+            $id_item,
+            $id_membre
+        ]);
+        $stmt->setFetchMode(\PDO::FETCH_CLASS, Sold::class);
+        return $stmt->fetch();
+    }
+
+    /**
+     * Return the sold with matching id_item
+     */
+    public function getFromItem(int $id): array {
+        $stmt = $this->db->prepare('SELECT id_item, item.libelle AS item, id_membre, membre.name AS membre, quantity, refunded FROM item_membre JOIN item ON item.id = item_membre.id_item JOIN membre ON membre.id = item_membre.id_membre WHERE id_item = ?');
+        $stmt->execute([
+            $id
+        ]);
+        return $stmt->fetchAll(\PDO::FETCH_CLASS, Sold::class);
+    }
+
+    /**
+     * Return the sold with matching id_membre
+     */
+    public function getFromMembre(int $id): array {
+        $stmt = $this->db->prepare('SELECT id_item, item.libelle AS item, id_membre, membre.name AS membre, quantity, refunded FROM item_membre JOIN item ON item.id = item_membre.id_item JOIN membre ON membre.id = item_membre.id_membre WHERE id_item = ?');
+        $stmt->execute([
+            $id
+        ]);
+        return $stmt->fetchAll(\PDO::FETCH_CLASS, Sold::class);
+    }
+
+    /**
+     * Create a sold
+     */
+    public function sell(int $id_item, int $id_membre, int $quantity, int $refunded): int {
+        $stmt = $this->db->prepare('INSERT INTO item_membre(id_item, id_membre, quantity, refunded) VALUES (?,?,?,?)');
+        $stmt->execute([
+            $id_item,
+            $id_membre,
+            $quantity,
+            $refunded
+        ]);
+        return $this->db->lastInsertId();
+    }
+
+    /**
+     * Update the sold
+     */
+    public function update(int $id_item, int $id_membre, int $quantity, int $refunded): int {
+        $stmt = $this->db->prepare('UPDATE item_membre SET quantity = ?, refunded = ? WHERE id_item = ? AND id_membre = ?');
+        $stmt->execute([
+            $quantity,
+            $refunded,
+            $id_item,
+            $id_membre
+        ]);
+        return $stmt->rowCount();
+    }
+
+    /**
+     * Delete the sold
+     */
+    public function delete(int $id_item, int $id_membre): int {
+        $stmt = $this->db->prepare('DELETE FROM item_membre WHERE id_item = ? AND id_membre = ?');
+        $stmt->execute([
+            $id_item,
+            $id_membre
+        ]);
+        return $stmt->rowCount();
+    }
+}
